ffa933bb1bf57875b46fa5124d2976f3045f9984
mikeBlackman <michaelblackmanesq@gmail.com>
2015-08-26 01:01:01 +1200

    initial commit
    initial commit

---
+++ app/src/main/java/megacorp/animals/MainActivity.java
@@ -0,0 +1,51 @@
+package megacorp.animals;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.media.SoundPool;
+import android.media.SoundPool.OnLoadCompleteListener;
+
+public class MainActivity extends ActionBarActivity {
+
+    boolean loaded = false;
+    SoundPool soundPool;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SoundPool.Builder builder =  new SoundPool.Builder();
+        builder.setMaxStreams(25);
+
+        soundPool = builder.build();
+        soundPool.load(this, R.raw.cat, 1);
+        soundPool.load(this, R.raw.chicken2, 1);
+        soundPool.load(this, R.raw.cow, 1);
+        soundPool.load(this, R.raw.dog1, 1);
+        soundPool.load(this, R.raw.duck, 1);
+        soundPool.load(this, R.raw.horse, 1);
+        soundPool.load(this, R.raw.pig, 1);
+        soundPool.load(this, R.raw.sheep, 1);
+        soundPool.load(this, R.raw.turkey2, 1);
+
+        soundPool.setOnLoadCompleteListener(new OnLoadCompleteListener() {
+            public void onLoadComplete(SoundPool soundPool, int sampleId,int status) {
+                loaded = true;
+            }
+        });
+
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                if (loaded) {
+                    soundPool.play(position+1, 1.0f, 1.0f, 1, 0, 1.0f);
+                }
+            }
+        });
+    }
+}
