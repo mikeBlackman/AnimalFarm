7b21d8e8c771b6fb8fabd63f657a7a124c9dd30e
Mike <michaelblackmanesq@gmail.com>
2017-06-16 23:47:18 +1200

    Allow rotation, Store animal data in a model of sorts

--- app/src/main/java/megacorp/animals/MainActivity.java
+++ app/src/main/java/megacorp/animals/MainActivity.java
@@ -8,18 +8,25 @@ import android.widget.GridView;
 import android.media.SoundPool;
 import android.media.SoundPool.OnLoadCompleteListener;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     boolean loaded;
     SoundPool soundPool;
+    List<Animal> animals = new ArrayList<>();
+
+    // TODO Use a recyclerview, proper adapter etc
+    // Konvert to kotlin
+    // Add new images
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         soundPool = buildSoundPool();
-
+        buildAnimalData();
         GridView gridview = (GridView) findViewById(R.id.gridview);
         gridview.setAdapter(new ImageAdapter(this));
         gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -32,28 +39,30 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public SoundPool buildSoundPool(){
-
         SoundPool.Builder builder =  new SoundPool.Builder();
         builder.setMaxStreams(25);
-
         soundPool = builder.build();
-        soundPool.load(this, R.raw.cat, 1);
-        soundPool.load(this, R.raw.chicken2, 1);
-        soundPool.load(this, R.raw.cow, 1);
-        soundPool.load(this, R.raw.dog1, 1);
-        soundPool.load(this, R.raw.duck, 1);
-        soundPool.load(this, R.raw.horse, 1);
-        soundPool.load(this, R.raw.pig, 1);
-        soundPool.load(this, R.raw.sheep, 1);
-        soundPool.load(this, R.raw.turkey2, 1);
-
+        for (Animal animal : animals){
+            soundPool.load(this, animal.getSoundId(), 1);
+        }
         soundPool.setOnLoadCompleteListener(new OnLoadCompleteListener() {
             public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
                 loaded = true;
             }
         });
-
         return soundPool;
+    }
+
+    void buildAnimalData(){
+        animals.add(new Animal(R.raw.cat, R.drawable.cat));
+        animals.add(new Animal(R.raw.chicken2, R.drawable.chicken));
+        animals.add(new Animal(R.raw.cow, R.drawable.cow));
+        animals.add(new Animal(R.raw.dog1, R.drawable.dog));
+        animals.add(new Animal(R.raw.duck, R.drawable.duck));
+        animals.add(new Animal(R.raw.horse, R.drawable.horse));
+        animals.add(new Animal(R.raw.pig, R.drawable.pig));
+        animals.add(new Animal(R.raw.sheep, R.drawable.sheep));
+        animals.add(new Animal(R.raw.turkey2, R.drawable.turkey));
     }
 
 }
